43086c82602ef372d4e8cae4de7585a8ab5b49c4
Hamed Fathizadeh <hamed.fathizadeh@smail.inf.h-brs.de>
2020-06-08 19:31:13 +0200

    BewerbungWindow bearbeitet

--- src/main/java/org/bonn/se/gui/window/BewerbungWindow.java
+++ src/main/java/org/bonn/se/gui/window/BewerbungWindow.java
@@ -29,25 +29,48 @@ public class BewerbungWindow extends Window {
         panel.setWidthFull();
 
 
-        GridLayout gridLayout = new GridLayout(5, 14);
-        gridLayout.setSizeFull();
-        gridLayout.setMargin(true);
+        GridLayout mainGridLayout = new GridLayout(5, 14);
+        mainGridLayout.setSizeFull();
+        mainGridLayout.setMargin(true);
         Student student = (Student) UI.getCurrent().getSession().getAttribute(Roles.Student);
         System.out.println(student.toString());
 
-        Image logo = unternehmen_data.getLogo();
+        Image profilbild = student.getImage();
         Label titel = new Label("<h2><b> Bewerbung auf: " + stellenanzeige.getTitel() + "</font></b></h3>", ContentMode.HTML);
+        Label vor_nachname = new Label("<h2><b>" +student.getVorname()+" "+student.getNachname()+ "</font></b></h3>", ContentMode.HTML);
+        Label geb_datum = new Label("Geb Datum: "+student.getG_datum());
+        Label adresse = new Label("Adresse: "+student.getAdresse().getStrasse()+" "+student.getAdresse().getHaus_nr());
+        Label plzOrt = new Label("PLZ Ort: "+student.getAdresse().getPlz()+" "+student.getAdresse().getOrt());
+        Label rufnummer = new Label("Rufnummer: "+student.getMobil_nr());
+        Label email = new Label("Email: "+student.getEmail());
+        Label abschluss = new Label("Abschluss: "+student.getAbschluss());
+        Label studiengang = new Label("Studiengang: "+student.getStudiengang());
+        Label ausbildung = new Label("Ausbildung: "+student.getAusbildung());
 
 
-        gridLayout.addComponent(logo, 1, 1);
-        gridLayout.addComponent(titel, 1, 2, 4, 2);
+
+
+        mainGridLayout.addComponent(titel, 0, 1, 4, 1);
+        mainGridLayout.addComponent(profilbild, 0, 2,0,7);
+        mainGridLayout.addComponent(vor_nachname, 1, 2,1,2);
+        mainGridLayout.addComponent(geb_datum, 1, 3,1,3);
+        mainGridLayout.addComponent(adresse, 1, 4,1,4);
+        mainGridLayout.addComponent(plzOrt, 1, 5,1,5);
+        mainGridLayout.addComponent(rufnummer, 1, 6,1,6);
+        mainGridLayout.addComponent(email, 1, 7,1,7);
+
+        mainGridLayout.addComponent(abschluss, 2, 3,2,3);
+        mainGridLayout.addComponent(studiengang, 2, 4,2,4);
+        mainGridLayout.addComponent(ausbildung, 2, 5,2,5);
+
+
 
 
         Button bewerben = new Button("Bewerben");
         Button back = new Button("Zurück zu Ergebnissen");
 
-        gridLayout.addComponent(bewerben, 4, 13, 4, 13);
-        gridLayout.addComponent(back, 4, 0, 4, 0);
+        mainGridLayout.addComponent(bewerben, 4, 13, 4, 13);
+        mainGridLayout.addComponent(back, 4, 0, 4, 0);
 
 
         back.addClickListener(new Button.ClickListener() {
@@ -66,8 +89,8 @@ public class BewerbungWindow extends Window {
         sample.setImmediateMode(false);
         sample.setButtonCaption("Upload File");
 
-        gridLayout.addComponent(sample, 3, 13);
-        panel.setContent(gridLayout);
+        mainGridLayout.addComponent(sample, 3, 13);
+        panel.setContent(mainGridLayout);
         this.setContent(panel);
 
         UploadInfoWindow  uploadInfoWindow = new UploadInfoWindow(sample, lineBreakCounter);
