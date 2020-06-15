d445af9351a1aff883784f01fb2e355f8bf1ad1f
Antonio Moscato <antonio.moscato@smail.inf.h-brs.de>
2020-06-15 13:19:28 +0200

    Testklasse "TestImageConverter" erstellt, noch nicht gefüllt

---
+++ src/test/java/junit/TestImageConverter.java
@@ -0,0 +1,29 @@
+package junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestImageConverter {
+
+    @Test
+    void getInt() {
+        assertEquals(2,2);
+    }
+
+    @Test
+    void getUnknownProfilImage() {
+    }
+
+    @Test
+    void getUnknownMenuImage() {
+    }
+
+    @Test
+    void convertImagetoProfil() {
+    }
+
+    @Test
+    void convertImagetoMenu() {
+    }
+}
