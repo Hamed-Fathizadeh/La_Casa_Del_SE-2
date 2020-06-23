47759b324c1ede2969f806a648e501f970492fc4
Tobias Fellechner <tobias.fellechner@smail.inf.h-brs.de>
2020-06-23 22:00:26 +0200

    Packages geändert

--- src/test/java/junit/BewerbungDAOTest.java
+++ src/test/java/junit/BewerbungDAOTest.java
@@ -1,15 +1,11 @@
 
 package junit;
-import org.bonn.se.model.objects.dto.BewerbungDTO;
+import org.bonn.se.model.dao.BewerbungDAO;
 import org.bonn.se.services.db.exception.DatabaseException;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.bonn.se.model.dao.BewerbungDAO;
 
 import java.sql.SQLException;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class BewerbungDAOTest {
     @Test
@@ -23,5 +19,4 @@ public class BewerbungDAOTest {
              Assertions.assertFalse(bewerb.markierungAendern(2));
         Assertions.assertNull(bewerb.downloadLebenslauf(16));
     }
-
 }
