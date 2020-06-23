c75e4e86497391f69841344a19e4c3082685f814
shasso2s <hassouni.salima@outlook.com>
2020-06-23 16:25:01 +0200

    FeatureToggleControlDao gestestet

--- src/test/java/junit/util/FeatureToggleControlTest.java
+++ src/test/java/junit/util/FeatureToggleControlTest.java
@@ -1,17 +1,22 @@
 package junit.util;
 import org.bonn.se.control.FeatureToggleControl;
+import org.bonn.se.model.dao.FeatureToggleDAO;
+import org.bonn.se.services.db.exception.DatabaseException;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+
+import java.sql.SQLException;
 
 public class FeatureToggleControlTest {
 
     @Test
-    public void testFeatureControl(){
+    public void testFeatureControl() throws DatabaseException, SQLException {
         FeatureToggleControl featureToggleControl= FeatureToggleControl.getInstance();
         Assertions.assertFalse(featureToggleControl.featureIsEnabled("fh"));
         Assertions.assertTrue(featureToggleControl.featureIsEnabled("BEWERBUNGEN"));
 
-
+        FeatureToggleDAO featureToggleDAO = FeatureToggleDAO.getInstance();
+        Assertions.assertTrue(featureToggleDAO.featureIsEnabled("BEWERBUNGEN"));
     }
 
 }
