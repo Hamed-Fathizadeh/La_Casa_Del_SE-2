790210a852a46197970e6486dab53324b3504cdb
Tobias Fellechner <tobias.fellechner@smail.inf.h-brs.de>
2020-06-30 18:13:20 +0200

    pom

--- src/main/java/org/bonn/se/gui/ui/MyUI.java
+++ src/main/java/org/bonn/se/gui/ui/MyUI.java
@@ -51,7 +51,7 @@ public class MyUI extends UI {
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MyUI.class, productionMode = true)
+    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
     }
 }
