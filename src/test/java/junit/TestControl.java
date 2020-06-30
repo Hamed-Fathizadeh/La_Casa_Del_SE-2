9ffe7ed8a68adc29ff8cef6146960c605749dcad
Tobias Fellechner <tobias.fellechner@smail.inf.h-brs.de>
2020-06-30 23:14:01 +0200

    Util Tests

--- src/test/java/junit/TestControl.java
+++ src/test/java/junit/TestControl.java
@@ -1,7 +1,11 @@
 package junit;
 
-import org.bonn.se.control.SucheControl;
+import org.bonn.se.control.*;
+import org.bonn.se.model.objects.dto.StellenanzeigeDTO;
+import org.bonn.se.services.db.exception.DatabaseException;
 import org.junit.Test;
+
+import java.sql.SQLException;
 
 public class TestControl {
 
@@ -12,4 +16,25 @@ public class TestControl {
         sucheControl.einfacheSuche("Physiker","Bonn","Nordrhein-Westfalen","Ganzer Ort","Erweitert",null,null,null);
 
     }
+
+    @Test
+    public void controlComponent() {
+        ComponentControl.getInstance().getAbschluss();
+        ComponentControl.getInstance().getBranche();
+        ComponentControl.getInstance().getBund();
+        ComponentControl.getInstance().getOrt();
+        ComponentControl.getInstance().getEinstellungsArt();
+        ComponentControl.getInstance().getITNiveau();
+        ComponentControl.getInstance().getSpracheNiveau();
+        ComponentControl.getInstance().getSuchbegriffe();
+    }
+
+    @Test
+    public void controlUnternehmenDescription() throws DatabaseException, SQLException {
+        JobTitelControl.getJobTitelList();
+        AnzStatusControl.changeStatus(new StellenanzeigeDTO());
+        UserSearchControl.getInstance().existUser("tobias.fellechner@365h-brs.de");
+    }
+
+
 }
