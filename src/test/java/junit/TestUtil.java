fc497ba9f2568f3591047728ff9307610005dddb
Tobias Fellechner <tobias.fellechner@smail.inf.h-brs.de>
2020-06-30 22:53:47 +0200

    Util Tests

--- src/test/java/junit/TestUtil.java
+++ src/test/java/junit/TestUtil.java
@@ -1,7 +1,6 @@
 package junit;
 
-import org.bonn.se.services.util.BrancheService;
-import org.bonn.se.services.util.DTOFactory;
+import org.bonn.se.services.util.*;
 import org.junit.Test;
 
 public class TestUtil {
@@ -20,5 +19,35 @@ public class TestUtil {
     public void utilDTOFactory() {
         DTOFactory.createBewerbungDTO(1,null,"Test",null ,1,1,1,"name","bonn"
                 ,null,"email","titel",null,"studium","ausbildung","konakt",1,null,"abschluss","vorname","nachname",0.0,true);
+    }
+    @Test
+    public void utilPDFUploader(){
+        PdfUploader.getByte();
+    }
+    @Test
+    public void utilSuchbegriffService(){
+        SuchbegrifService suchbegrifService = new SuchbegrifService();
+        suchbegrifService.getSuchbegriffe();
+        suchbegrifService.count("");
+        suchbegrifService.fetch("",0,5);
+        suchbegrifService.count(null);
+        suchbegrifService.fetch(null,0,5);
+    }
+
+    @Test
+    public void utilVerificationNummer(){
+        VerifikationNummer verifikationNummer =new VerifikationNummer();
+
+        VerifikationNummer.getInstance().setRandNummer();;
+        VerifikationNummer.getInstance().getRandNummer();
+    }
+
+    @Test
+    public void utilClasses(){
+        Roles roles = new Roles();
+        Views views = new Views();
+        Password password = new Password();
+        IllegalException illegalException = new IllegalException();
+
     }
 }
